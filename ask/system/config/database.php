3b1b3f40f69dd05e06e161fbe8dd731779dc45c0
kelaocai <kelaocai@163.com>
2013-10-31 21:55:54 +0800

    安装wecenter
    开发环境安装wecenter

---
+++ ask/system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'ask_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => '3edc4rfv',
+  'dbname' => 'tsbdev',
+);
+$config['slave'] = false;
